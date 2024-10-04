9b6d5519fab676e753afd2d749b68694ab094acc
sametatabasch <sametatabasch@gmail.com>
2024-10-04 17:05:46 +0300

    index.php dosyasında html şablonu eklendi

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Wep Projesi Dersi</title>
+</head>
+<body>
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores, cum quod. Provident veritatis deleniti corporis cum. Optio voluptates amet iure, quasi, enim nisi beatae facere in quibusdam atque fugiat harum?
+</body>
+</html>
